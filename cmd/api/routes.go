373c2373e9235bf7c7110f7a373806c454703579
Sarath191181208 <vssarathc04@gmail.com>
2024-10-02 22:28:55 +0530

    +prometheus and grafana

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,6 +6,7 @@ import (
 	"sarath/perf_testing/cmd/api/services/users"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func (app *Application) Routes() *mux.Router{
@@ -17,6 +18,7 @@ func (app *Application) Routes() *mux.Router{
   mux.HandleFunc("/health", health_check_handler.HandleHealthCheck)
   mux.HandleFunc("/register", users_handler.RegisterUsers).Methods(http.MethodPost)
   mux.HandleFunc("/get/{id}", users_handler.FindUser)
+  mux.Handle("/metrics", promhttp.Handler())
 
   return mux
 }
